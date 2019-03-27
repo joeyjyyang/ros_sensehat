ef6f28bbdfd4eea6f864a1b115720c4ee8a6d912
joeyjyyang <joeyyang.ai@gmail.com>
2019-03-26 23:36:55 -0400

    added custom IMU orientation messages

--- src/display_orientation.py
+++ src/display_orientation.py
@@ -1,30 +1,24 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import String
+from sensehat_driver.msg import IMUOrientation
 from sense_hat import SenseHat
 
-blue = (0, 0, 255)
-yellow = (255, 255, 0)
-red = (255, 0, 0)
-green = (0, 255, 0)
-
 def display_callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    direction = float(data.data)
+    rospy.loginfo("yaw: %f" % data.yaw)
     
-    if (45 <= direction < 135):
+    if (45 <= data.raw < 135):
         sense.show_letter("E")
-    elif (135 <= direction < 225):
+    elif (135 <= data.yaw < 225):
         sense.show_letter("S")
-    elif (225 <= direction < 315):
+    elif (225 <= data.yaw < 315):
         sense.show_letter("W")
     else:
         sense.show_letter("N")
 
 def sub_orientation():
     rospy.init_node("display_orientation", anonymous=True)
-    rospy.Subscriber("imu_orientation", String, display_callback)
+    rospy.Subscriber("imu_orientation", IMUOrientation, display_callback)
     rospy.spin()
 
 if __name__ == '__main__':
--- src/imu_read.py
+++ src/imu_read.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import String
+#from std_msgs.msg import String
+from sensehat_driver.msg import IMUOrientation #include custom IMU orientation messge 
 from sense_hat import SenseHat
 
 def pub_orientation():
-    pub = rospy.Publisher("imu_orientation", String, queue_size=10)
+    pub = rospy.Publisher("imu_orientation", IMUOrientation, queue_size=10)
     rospy.init_node("imu_read", anonymous=True)
     rate = rospy.Rate(30)
-    
+    imu_orientation = IMUOrientation()
+
     while not rospy.is_shutdown():
         orientation = sense.get_orientation()
-        direction = orientation["yaw"]
-        imu_reading = "%s" % direction
-        rospy.loginfo(imu_reading)
-        pub.publish(imu_reading)
+        imu_orientation.pitch = orientation["pitch"]
+        imu_orientation.roll = orientation["roll"]
+        imu_orientation.yaw = orientation["yaw"]
+        rospy.loginfo(imu_orientation)
+        pub.publish(imu_orientation)
         rate.sleep()
 
 if __name__ == '__main__':
     sense = SenseHat()
     sense.set_imu_config(True, True, True)
+    
     try:
         pub_orientation()
     except rospy.ROSInterruptException:
