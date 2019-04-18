048a9e812869175314eae6221e95c376e8cf34da
joeyjyyang <joeyyang.ai@gmail.com>
2019-04-17 23:55:31 -0400

    functional quaternion class import and quaternion data publishing

--- scripts/sensehat_imu_publisher.py
+++ scripts/sensehat_imu_publisher.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
-#import sys
-#sys.path.insert(0, "../src")
-
-#import sensehat_driver.sensehat_imu
 from sensehat_driver import sensehat_imu
-
 from sensehat_driver.msg import IMUOrientation 
 from sensor_msgs.msg import Imu
 #from geometry_msgs.msg import Quaternion, Vector3
@@ -19,35 +14,40 @@ class SenseHatIMUPublisherNode:
         self.imu_data_msg = Imu()
         self.imu_euler_msg = IMUOrientation() 
         
-        #self.pub_imu_data = rospy.Publisher("imu_data", Imu, queue_size=1)
+        self.imu_data_publisher = rospy.Publisher("imu_data", Imu, queue_size=1)
         self.imu_euler_publisher = rospy.Publisher("imu_euler", IMUOrientation, queue_size=1)
 
     def pub_imu_data(self):
-#        self.orientation_quaternion = self.sensehat_imu.euler_to_quaternion()
+         self.orientation_quaternion = self.sensehat_imu.euler_to_quaternion()
          self.orientation_euler = self.sensehat_imu.get_orientation_degrees()
-#        self.angular_velocity = self.sensehat_imu.angular_velocity()
-#        self.linear_acceleration = self.sensehat_imu.linear_acceleration()
-#
-#        self.imu_data_msg.orientation.x = self.orientation_quaternion.quaternion_x
-#        self.imu_data_msg.orientation.y = self.orientation_quaternion.quaternion_y
-#        self.imu_data_msg.orientation.z = self.orientation_quaternion.quaternion_z
-#        self.imu_data_msg.orientation.w = self.orientation_quaternion.quaternion_w
-#
+         self.angular_velocity = self.sensehat_imu.angular_velocity()
+         self.linear_acceleration = self.sensehat_imu.linear_acceleration()
+
+         #self.imu_data_msg.orientation.x = self.orientation_quaternion.quaternion_x
+         #self.imu_data_msg.orientation.y = self.orientation_quaternion.quaternion_y
+         #self.imu_data_msg.orientation.z = self.orientation_quaternion.quaternion_z
+         #self.imu_data_msg.orientation.w = self.orientation_quaternion.quaternion_w
+         
+         self.imu_data_msg.orientation.x = self.orientation_quaternion.quaternion_x
+         self.imu_data_msg.orientation.y = self.orientation_quaternion.quaternion_y
+         self.imu_data_msg.orientation.z = self.orientation_quaternion.quaternion_z
+         self.imu_data_msg.orientation.w = self.orientation_quaternion.quaternion_w
+        
          self.imu_euler_msg.pitch = self.orientation_euler["pitch"]
          self.imu_euler_msg.roll = self.orientation_euler["roll"]
          self.imu_euler_msg.yaw = self.orientation_euler["yaw"]
 
-#        self.imu_data_msg.angular_velocity.x = self.angular_velocity["x"]
-#        self.imu_data_msg.angular_velocity.y = self.angular_velocity["y"]
-#        self.imu_data_msg.angular_velocity.z = self.angular_velocity["z"]
-#
-#        self.imu_data_msg.linear_acceleration.x = self.linear_acceleration["x"]
-#        self.imu_data_msg.linear_acceleration.y = self.linear_acceleration["y"]
-#        self.imu_data_msg.linear_acceleration.z = self.linear_acceleration["z"]
-#
-#        self.pub_imu_data.publish(self.imu_data_msg)
+         self.imu_data_msg.angular_velocity.x = self.angular_velocity["x"]
+         self.imu_data_msg.angular_velocity.y = self.angular_velocity["y"]
+         self.imu_data_msg.angular_velocity.z = self.angular_velocity["z"]
+
+         self.imu_data_msg.linear_acceleration.x = self.linear_acceleration["x"]
+         self.imu_data_msg.linear_acceleration.y = self.linear_acceleration["y"]
+         self.imu_data_msg.linear_acceleration.z = self.linear_acceleration["z"]
+
+         self.imu_data_publisher.publish(self.imu_data_msg)
          self.imu_euler_publisher.publish(self.imu_euler_msg)
-#
+
 if __name__ == '__main__':
     rospy.loginfo("Publishing Sense Hat IMU data.")
 
--- src/sensehat_driver/sensehat_imu.py
+++ src/sensehat_driver/sensehat_imu.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import math
-#from sensehat_quaternion import Quaternion
+from sensehat_quaternion import Quaternion
 from sense_hat import SenseHat
 
 class SenseHatIMU(SenseHat): 
@@ -37,14 +37,14 @@ class SenseHatIMU(SenseHat):
         quaternion_z = sin_yaw * cos_pitch * cos_roll - cos_yaw * sin_pitch * sin_roll 
         quaternion_w = cos_yaw * cos_pitch * cos_roll + sin_yaw * sin_pitch * sin_roll
 
-        #self.orientation_quaternion = Quaternion(quaternion_x, quaternion_y, quaternion_z, quaternion_w)
+        self.orientation_quaternion = Quaternion(quaternion_x, quaternion_y, quaternion_z, quaternion_w)
         
-        self.orientation_quaternion = dict()
+        #self.orientation_quaternion = dict()
 
-        self.orientation_quaternion["x"] = quaternion_x
-        self.orientation_quaternion["y"] = quaternion_y
-        self.orientation_quaternion["z"] = quaternion_z
-        self.orientation_quaternion["w"] = quaternion_w
+        #self.orientation_quaternion["x"] = quaternion_x
+        #self.orientation_quaternion["y"] = quaternion_y
+        #self.orientation_quaternion["z"] = quaternion_z
+        #self.orientation_quaternion["w"] = quaternion_w
 
         return self.orientation_quaternion
 
