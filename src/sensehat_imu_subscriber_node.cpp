6fd4430c4e266e19a31ede2f566f132f061660f3
joeyjyyang <joeyyang.ai@gmail.com>
2019-06-28 02:26:56 -0400

    dummy push

--- src/sensehat_imu_subscriber_node.cpp
+++ src/sensehat_imu_subscriber_node.cpp
@@ -1,7 +1,7 @@
 #include "sensehat_driver/sensehat_imu_subscriber.h"
 
-int main(int argc, char *argv[]) {
-	
+int main(int argc, char *argv[]) 
+{	
 	ros::init(argc, argv, "sensehat_imu_subscriber_node");
 	ros::NodeHandle nh;
 	sensehat_imu::SenseHatIMUSubscriber sensehat_imu_subscriber(nh);
