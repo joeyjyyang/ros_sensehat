c506684e773f29493474e490e4d05627c032e82c
joeyjyyang <joeyyang.ai@gmail.com>
2019-08-22 00:23:34 -0400

    created generic imu launch

--- src/sensehat_imu_subscriber.cpp
+++ src/sensehat_imu_subscriber.cpp
@@ -7,9 +7,8 @@ sensehat_imu::SenseHatIMUSubscriber::SenseHatIMUSubscriber(const ros::NodeHandle
 
 void sensehat_imu::SenseHatIMUSubscriber::IMUCallback(const sensor_msgs::Imu::ConstPtr& imu_msg) 
 {
-
-	ROS_INFO("Quaternions: x=[%f], y=[%f], z=[%f], w=[%f]", imu_msg->orientation.x, imu_msg->orientation.y, imu_msg->orientation.z, imu_msg->orientation.w);
 	/*
+		ROS_INFO("Quaternions: x=[%f], y=[%f], z=[%f], w=[%f]", imu_msg->orientation.x, imu_msg->orientation.y, imu_msg->orientation.z, imu_msg->orientation.w);
 		Do something with data (i.e. get yaw/heading vs. time stamp and output warning if turning too fast)
 	*/
 }
