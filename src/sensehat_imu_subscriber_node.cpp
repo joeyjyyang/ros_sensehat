adf8efb5d783c33b45e7ca22c2225146ce844095
Joey Yang <joeyyang.ai@gmail.com>
2020-08-24 00:46:50 -0400

    update namespace

--- src/sensehat_imu_subscriber_node.cpp
+++ src/sensehat_imu_subscriber_node.cpp
@@ -1,11 +1,11 @@
 #include "sensehat_driver/sensehat_imu_subscriber.h"
 
-int main(int argc, char *argv[]) {
-	
-	ros::init(argc, argv, "sensehat_imu_subscriber_node");
-	ros::NodeHandle nh;
-	sensehat_imu::SenseHatIMUSubscriber sensehat_imu_subscriber(nh);
-	ros::spin();
+int main(int argc, char *argv[]) 
+{
+  ros::init(argc, argv, "sensehat_imu_subscriber_node");
+  ros::NodeHandle nh;
+  sensehat_driver::SenseHatIMUSubscriber sensehat_imu_subscriber(nh);
+  ros::spin();
 
-	return 0;
+  return 0;
 }
