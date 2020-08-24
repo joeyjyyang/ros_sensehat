f437fb3cfd27979ee5f1d3303934bf6e39d340ae
Joey Yang <joeyyang.ai@gmail.com>
2020-08-24 00:46:10 -0400

    update namespace

--- include/sensehat_driver/sensehat_imu_subscriber.h
+++ include/sensehat_driver/sensehat_imu_subscriber.h
@@ -7,21 +7,18 @@
 #include "ros/ros.h"
 #include "sensor_msgs/Imu.h"
 
-namespace sensehat_imu 
+namespace sensehat_driver
 {
 class SenseHatIMUSubscriber 
 {
 public:
-	explicit SenseHatIMUSubscriber(const ros::NodeHandle& nh);
-	
-	void IMUCallback(const sensor_msgs::Imu::ConstPtr& imu_msg);
-
-	~SenseHatIMUSubscriber();
-
+  explicit SenseHatIMUSubscriber(const ros::NodeHandle& nh);
+  void IMUCallback(const sensor_msgs::Imu::ConstPtr& imu_msg);
+  ~SenseHatIMUSubscriber();
 private:
-	ros::Subscriber sensehat_imu_subscriber_;
-	ros::NodeHandle nh_;
+  ros::Subscriber sensehat_imu_subscriber_;
+  ros::NodeHandle nh_;
 };
-}
+} // namespace sensehat_driver
 
 #endif // SENSEHAT_IMU_SUBSCRIBE_H
