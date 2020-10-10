32ad2a8820a963c1ebaec4c8ea4b72477facf576
Joey Yang <yangj30@mcmaster.ca>
2020-10-09 22:23:34 -0400

    update package name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-        packages=['sensehat_driver'],
+        packages=['ros_sensehat'],
         package_dir={'': 'src'}
 )
 
